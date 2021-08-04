e1e8ddda727a8e4e843c7c6497a6cb2a02702596
Valentin Laurin <valentin.laurin@quickcase.app>
2021-08-04 23:39:02 +0100

    Extract and test getPruningList()
    Relates to #3 and prepares for #4

---
+++ src/pruning.js
@@ -0,0 +1,27 @@
+const PAGE_SIZE = 100;
+
+const getPruningList = (listVersions, pruningFilter) => async () => {
+  let pruningList = [];
+  let page = 1;
+  let lastPageSize = 0;
+
+  console.log('Crawling through all versions to build pruning list...');
+
+  do {
+    const {data: versions} = await listVersions(page);
+    lastPageSize = versions.length;
+
+    const pagePruningList = versions.filter(pruningFilter);
+    pruningList = [...pruningList, ...pagePruningList];
+
+    console.log(`Found ${pagePruningList.length} versions to prune out of ${lastPageSize} on page ${page}`);
+
+    page++;
+  } while (lastPageSize >= PAGE_SIZE);
+
+  return pruningList;
+};
+
+module.exports = {
+  getPruningList,
+};
